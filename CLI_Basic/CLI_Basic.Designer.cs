0c749e2c7e0370f506f2acf1afca91076eee66a6
morepolys821 <81265808+morepolys821@users.noreply.github.com>
2021-10-27 20:14:34 -0700

    Initial commit

---
+++ CLI_Basic/CLI_Basic.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace CLI_Basic
+{
+    partial class CLI_Basic
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.TxtInputFolder = new System.Windows.Forms.TextBox();
+            this.TxtOutputFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnExecute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExecute.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExecute.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExecute.Location = new System.Drawing.Point(25, 342);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(273, 33);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = false;
+            this.btnExecute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtInputFolder
+            // 
+            this.TxtInputFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.TxtInputFolder.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtInputFolder.Font = new System.Drawing.Font("Inter", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtInputFolder.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtInputFolder.Location = new System.Drawing.Point(25, 214);
+            this.TxtInputFolder.Name = "TxtInputFolder";
+            this.TxtInputFolder.Size = new System.Drawing.Size(273, 16);
+            this.TxtInputFolder.TabIndex = 1;
+            this.TxtInputFolder.Text = "C:\\temp\\clidemo\\input";
+            // 
+            // TxtOutputFolder
+            // 
+            this.TxtOutputFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.TxtOutputFolder.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtOutputFolder.Font = new System.Drawing.Font("Inter", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtOutputFolder.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.TxtOutputFolder.Location = new System.Drawing.Point(25, 299);
+            this.TxtOutputFolder.Name = "TxtOutputFolder";
+            this.TxtOutputFolder.Size = new System.Drawing.Size(273, 16);
+            this.TxtOutputFolder.TabIndex = 2;
+            this.TxtOutputFolder.Text = "C:\\temp\\clidemo\\output";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(25, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Inter", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(263, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CLI Demo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Inter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Silver;
+            this.label3.Location = new System.Drawing.Point(25, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output Folder";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(314, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(314, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CLI_Basic
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(363, 413);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtOutputFolder);
+            this.Controls.Add(this.TxtInputFolder);
+            this.Controls.Add(this.btnExecute);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name = "CLI_Basic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ArtEngine CLI Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox TxtInputFolder;
+        private System.Windows.Forms.TextBox TxtOutputFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CLI_Basic/CLI_Basic.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace CLI_Basic
+{
+    public partial class CLI_Basic : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+        public CLI_Basic()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            execute();
+        }
+
+        private void execute()
+        {
+            var myList = new List<string>();
+
+            foreach (var files in System.IO.Directory.GetFiles(TxtInputFolder.Text, "*.jpg"))
+            {
+                /* This goes through the directory and searches for .jpg and will loop these processes */
+                /* The config file location is hardcoded for the time being */
+
+                
+                // Defining files and paths based on input files
+                string fileNoPath = Path.GetFileName(files);
+                string fileNoExtension = Path.GetFileNameWithoutExtension(files);
+                string fileOutDir = TxtOutputFolder.Text + @"\" + fileNoExtension;
+
+                // Create a directory to output results to
+                Directory.CreateDirectory(TxtOutputFolder.Text + @"\" + fileNoExtension);
+
+                /* Setting up config XML file */
+                string cliDemoConfig = @"C:\temp\clidemo\cli_video-config.xml";
+                String[] masklines = File.ReadAllLines(cliDemoConfig);
+
+                ////////////////////////* Replace the $tokens with the input files */
+                for (int i = 1; i <= 49; i++)
+                {
+                    masklines[i - 1] = masklines[i - 1].Replace("$INPUT", files);
+                    masklines[i - 1] = masklines[i - 1].Replace("$OUTPUTDIR", fileOutDir);
+                    masklines[i - 1] = masklines[i - 1].Replace("$OUTPUT", fileNoPath);
+                }
+
+                File.WriteAllLines(cliDemoConfig, masklines);
+
+                //MessageBox.Show("edited");
+                ////////////////////* RUN ARTENGINE HERE */
+
+                string artEngineConfig = "cli_video-config.xml";
+                
+                //Basepath for config xml
+                string configxml = @"C:\temp\clidemo" + artEngineConfig;
+
+                //Params for executable        
+                string artEngine = @"--config C:\temp\clidemo\" + artEngineConfig;
+
+                //Execute
+                var process = Process.Start(@"C:\Program Files\Artomatix\Artomatix\ArtEngine.CLI\ArtEngine.CLI.exe", artEngine);
+                process.WaitForExit(); 
+                
+
+
+                /////////////////////////////* Resetting the config file back to tokens */
+                for (int i = 1; i <= 49; i++)
+                {
+                    masklines[i - 1] = masklines[i - 1].Replace(files, "$INPUT");
+                    masklines[i - 1] = masklines[i - 1].Replace(fileOutDir, "$OUTPUTDIR");
+                    masklines[i - 1] = masklines[i - 1].Replace(fileNoPath, "$OUTPUT");
+
+                }
+
+                File.WriteAllLines(cliDemoConfig, masklines);
+                
+            }
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                TxtInputFolder.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                TxtOutputFolder.Text = fbd.SelectedPath;
+            }
+        }
+    }
+}
